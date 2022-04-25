da55e872b0eaf20d785be0a43077d5fbf4bfa86d
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-04-25 15:59:01 +0500

    Create a function to return a random hand

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-let computerPlay = Math.ceil(Math.random() * 3);
+const hands = ["rock", "paper", "scissors"]
 
-if (computerPlay === 1) {
-  computerPlay = 'rock';
-} else if (computerPlay === 2) {
-  computerPlay = 'paper';
-} else {
-  computerPlay = 'scissors';
+function randomHand() {
+    const randomNum = Math.floor(Math.random() * hands.length);
+    return hands[randomNum];
 }
 
-const userChoice = prompt('Choose "Rock", "Paper" or "Scissors"');
-const userPlay = userChoice.toLowerCase();
-
-if (userPlay === 'rock' && computerPlay === 'scissors') {
-  alert('You won! Rock beats scissors!');
-} else if (userPlay === 'rock' && computerPlay === 'paper') {
-  alert('You lost! Paper beats rock!');
-} else if (userPlay === 'paper' && computerPlay === 'rock') {
-  alert('You won! Paper beats rock!');
-} else if (userPlay === 'paper' && computerPlay === 'scissors') {
-  alert('You lost! Scissors beats paper!');
-} else if (userPlay === 'scissors' && computerPlay === 'paper') {
-  alert('You won! Scissors beats paper!');
-} else if (userPlay === 'scissors' && computerPlay === 'rock') {
-  alert('You lost! Rock beats scissors!');
-} else if ((userPlay === 'rock' && computerPlay === 'rock') || (userPlay === 'paper' && computerPlay === 'paper') || (userPlay === 'scissors' && computerPlay === 'scissors')) {
-  alert('Tie game!');
-} else {
-  alert('Error! Invalid input!');
-}
+console.log(randomHand());
