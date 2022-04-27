b1cf5a7203a3acd81d8e26b0357c0cdaf04aef37
Ibrohim Rasulov <R.Ibrohim@192.168.1.7>
2022-04-27 14:19:22 +0500

    Update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const hands = ["rock", "paper", "scissors"]
+const hands = ["rock", "paper", "scissors"];
 
 function randomHand() {
-    const randomNum = Math.floor(Math.random() * hands.length);
-    return hands[randomNum];
+  const randomNum = Math.floor(Math.random() * hands.length);
+  return hands[randomNum];
 }
-
+ç
 console.log(randomHand());
