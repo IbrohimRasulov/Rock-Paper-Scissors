f7c42df03ab1a8d370f8a4df89ef139a5457850d
Ibrohim <ibrohim05062000@gmail.com>
2022-04-30 12:39:36 +0500

    Create a function to randomly choose a hand for computerPlay, and add a function to check the result

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const hands = ["rock", "paper", "scissors"];
+const playerSelection = "rock";
 
-function randomHand() {
+
+function computerPlay() {
   const randomNum = Math.floor(Math.random() * hands.length);
   return hands[randomNum];
 }
-ç
-console.log(randomHand());
+
+const playRound = function (playerSelection, computerSelection) {
+  if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    console.log('You won! Rock beats scissors!');
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    console.log('You lost! Paper beats rock!');
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    console.log('You won! Paper beats rock!');
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    console.log('You lost! Scissors beats paper!');
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    console.log('You won! Scissors beats paper!');
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    console.log('You lost! Rock beats scissors!');
+  } else if ((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
+    console.log('Tie game!');
+  } else {
+    console.log('Error! Invalid input!');
+  }
+};
+
+playRound(playerSelection, computerPlay());
